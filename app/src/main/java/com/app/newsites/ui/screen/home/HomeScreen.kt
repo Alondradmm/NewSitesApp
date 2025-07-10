64218c7ddd7952dc9702d433ad81ed3116f6a2e3
Alondradmm <dalon0904@gmail.com>
2025-07-09 19:10:56 -0600

    Bugs de Registro arreglados

--- app/src/main/java/com/app/newsites/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/app/newsites/ui/screen/home/HomeScreen.kt
@@ -1,9 +1,88 @@
 package com.app.newsites.ui.screen.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.newsites.R
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
-) {}
+    /*
+    navController: NavController,
+    viewModel: LoginViewModel = viewModel()
+    */
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 50.dp, vertical = 16.dp)
+            .systemBarsPadding()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "NewSites",
+                    fontFamily = FontFamily(Font(R.font.alata)),
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Red,
+                )
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(5.dp)
+                ){
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp),
+                        text = "Bienvenido",
+                        fontFamily = FontFamily(Font(R.font.alata)),
+                        fontSize = 20.sp,
+                    )
+
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp),
+                        text = "Alondradmm",
+                        fontFamily = FontFamily(Font(R.font.alata)),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+            }
+
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen()
+}
