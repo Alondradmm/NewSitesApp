34fe19e307e6bde6e5b22fd69d8710323011d4ce
Alondradmm <dalon0904@gmail.com>
2025-08-12 22:37:32 -0600

    Rating y Cambios Varios Tuve que modificar la lógica para mostrar los datos en el historial de HomeScreen.kt ya que necesitaba que el historial pudiera enviar un id para enviar el rating, además de añadir rating a cada visita del historial
    Algunos cambios en MapScreen
    - Se reintegró la lógica para añadir Sites (eliminada por error?)
    -

--- app/src/main/java/com/app/newsites/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/app/newsites/ui/screen/home/HomeViewModel.kt
@@ -7,8 +7,8 @@ import androidx.lifecycle.viewModelScope
 import com.app.newsites.data.DataStoreClass
 import com.app.newsites.data.SessionManager
 import com.app.newsites.data.repository.UserHistoryRepository
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -25,7 +25,7 @@ class HomeViewModel : ViewModel() {
 
     val _userHistory = MutableStateFlow<Map<String, List<Map<String, String>>>>(emptyMap())
     val userHistory: StateFlow<Map<String, List<Map<String, String>>>> = _userHistory
-    var userHistoryIds: Map<String, List<String>> = emptyMap()
+    var userHistoryIds: Map<String, List<Triple<String, Int, Int>>> = emptyMap()
 
     fun inicializarUsuario(userId: String, context: Context) {
         viewModelScope.launch {
@@ -37,25 +37,25 @@ class HomeViewModel : ViewModel() {
 
     fun cargarUsuario(userId: String) {
         viewModelScope.launch {
-            val doc = repository.getUserData(userId)
-            if (doc != null) {
-                _usuario.value = mapOf(
-                    "email" to userId,
-                    "username" to doc["username"].toString(),
-                    "phone" to doc["phone"].toString(),
-                    "points" to doc["points"].toString(),
-                    "totalSites" to doc["totalSites"].toString(),
-                    "daySites" to doc["daySites"].toString(),
-                    "newSites" to doc["newSites"].toString(),
-                    "history" to doc["history"],
-                    // "favoritos" to doc.get("favoritos"),
-                )
+            repository.getUserData(userId).collect { doc ->
+                if (doc != null) {
+                    _usuario.value = mapOf(
+                        "email" to userId,
+                        "username" to doc["username"].toString(),
+                        "phone" to doc["phone"].toString(),
+                        "points" to doc["points"].toString(),
+                        "totalSites" to doc["totalSites"].toString(),
+                        "daySites" to doc["daySites"].toString(),
+                        "newSites" to doc["newSites"].toString(),
+                        "history" to doc["history"],
+                    )
 
-                userHistoryIds = repository.getUserHistoryGrouped(userId)
-                SessionManager.userHistory = userHistoryIds
+                    userHistoryIds = repository.getUserHistoryGrouped(userId)
+                    SessionManager.userHistory = userHistoryIds
 
-                if (userHistoryIds.isNotEmpty()) {
-                    cargarHistorialUsuario()
+                    if (userHistoryIds.isNotEmpty()) {
+                        cargarHistorialUsuario()
+                    }
                 }
             }
 
@@ -69,15 +69,16 @@ class HomeViewModel : ViewModel() {
             val historyByDate = mutableMapOf<String, List<Map<String, String>>>()
 
             userHistoryIds.forEach { (date, sites) ->
+                val siteIds = sites.map { it.first }
                 val snapshot = FirebaseFirestore.getInstance()
                     .collection("sites")
-                    .whereIn(FieldPath.documentId(), sites)
+                    .whereIn(FieldPath.documentId(), siteIds)
                     .get()
                     .await()
 
                 val sitiosPorId = snapshot.documents.associateBy { it.id }
 
-                val listaOrdenada = sites.mapNotNull { siteId ->
+                val listaOrdenada = sites.mapNotNull { (siteId , rate , index) ->
                     val doc = sitiosPorId[siteId]
                     if (doc != null) {
                         val nombre = doc.getString("nombre")
@@ -91,7 +92,9 @@ class HomeViewModel : ViewModel() {
                                 "nombre" to nombre,
                                 "direccion" to direccion,
                                 "descripcion" to descripcion,
-                                "img" to img
+                                "img" to img,
+                                "rate" to rate.toString(),
+                                "index" to index.toString()
                             )
                         } else null
                     } else null
@@ -103,6 +106,50 @@ class HomeViewModel : ViewModel() {
             _userHistory.value = historyByDate.toMap()
         }
 
+    }
+
+    fun sendRate(siteId: String, historyIndex: Int, rate: Double, userId: String) {
+        val db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("usuarios").document(userId)
+
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null && document.exists()) {
+                val history = document.get("history") as? MutableList<Map<String, Any>>
+                if (history != null && history.size > 1) {
+                    // Clona el map en la posición 1 y actualiza el "rate"
+                    val updatedItem = history[historyIndex].toMutableMap()
+                    updatedItem["rate"] = rate
+
+                    // Reemplaza el elemento en la posición 1
+                    history[historyIndex] = updatedItem
+
+                    // Actualiza el campo history completo
+                    docRef.update("history", history)
+                        .addOnSuccessListener {
+                            Log.d("FIRESTORE", "Campo history actualizado correctamente")
+                            db.collection("sites")
+                                .document(siteId)
+                                .update(
+                                    mapOf(
+                                        "visitasCalificadas" to FieldValue.increment(1),
+                                        "totalRate" to FieldValue.increment(rate),
+                                    )
+                                )
+                                .addOnSuccessListener {
+                                    Log.d("Firestore", "Elemento agregado correctamente")
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.e("Firestore", "Error al agregar elemento", e)
+                                }
+                        }
+                        .addOnFailureListener { e ->
+                            Log.e("FIRESTORE", "Error actualizando history", e)
+                        }
+                }
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("FIRESTORE", "Error obteniendo documento", exception)
+        }
     }
 
 }
